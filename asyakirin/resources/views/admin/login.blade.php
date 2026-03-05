adadbf5059fb1c0e5b6ee9e2938bf083c7e1abc1
kemaliksn <kemaliksan5@gmail.com>
2026-03-05 11:57:25 +0700

    Add logo dashboard

--- asyakirin/resources/views/admin/login.blade.php
+++ asyakirin/resources/views/admin/login.blade.php
@@ -302,10 +302,7 @@
 
     <div class="left-content">
         <div class="brand-logo">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-                <path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"/>
-                <polyline points="9 22 9 12 15 12 15 22"/>
-            </svg>
+            <img style="width:50px; margin-bottom:3px" src="{{ asset('icons/logomasjid.png') }}" alt="Logo Asy-Syaakiriin">
         </div>
 
         <h1 class="brand-name">ZIS Asy-Syaakiriin</h1>
